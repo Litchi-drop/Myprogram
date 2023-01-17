5968502f7b9b07f997103fee7747aff1c265046b
YYC <Yanyuchen@163.com>
2023-01-17 13:56:19 +0800

    add SQLServerConnection_UtilsHelp

---
+++ Myprogram/SQLServerConnection_UtilsHelp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myprogram
+{
+    /// <summary>
+    /// SQL
+    /// </summary>
+    public class SQLServerConnection_UtilsHelp
+    {
+
+        public void SQLConnection() 
+        {
+            string ConnString = @"Data Source = localhost; Initial Catalog = PerformanceAppraisalDb;User ID = YYC ;Password = 123456 ";
+            SqlConnection connection = new SqlConnection(ConnString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("数据库连接成功！");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("连接失败！" + ex.Message);
+            }
+            finally 
+            {
+                connection.Close();
+                Console.WriteLine("数据库成功关闭！");
+
+
+            }
+        }
+        
+        
+    }
+}
