3ef66685e42ba34cc6016a7e8c9631b3c4b11436
YYC <Yanyuchen@163.com>
2023-02-01 09:25:57 +0800

    No28

---
+++ Myprogram/LeetCode/No28.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myprogram.LeetCode
+{
+    public class No28
+    {
+        public int StrStr(string haystack, string needle) 
+        {
+            int i = 0;
+
+
+            return i;
+        }
+    }
+}
---
+++ Myprogram/LeetCode/No70.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myprogram.LeetCode
+{
+    class No70
+    {
+        public int ClimbStairs(int n) 
+        {
+            int num = 0;
+
+
+            return num;
+        
+        }
+    }
+}
